a3f6f83815155fe4b5117824fa6b6fcdaea2917b
NathanAzvdo <nathan.azevedo28@gmail.com>
2025-08-05 20:45:37 -0300

    basic components generated

---
+++ src/app/components/logged-out/landing-page-banner/landing-page-banner.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-page-banner',
+  imports: [],
+  templateUrl: './landing-page-banner.html',
+  styleUrl: './landing-page-banner.css'
+})
+export class LandingPageBanner {
+
+}
